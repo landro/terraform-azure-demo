6c436ac0c7c6eec39cab3e65fe4a383a74e61e13
Stefan Magnus Landrø <stefan.landro@gmail.com>
2017-06-10 14:17:49 +0200

    Variables and interpolation

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,22 @@
 # Will use DIGITALOCEAN_TOKEN environment variable
 provider "digitalocean" {}
 
+variable "nb_web_servers" {
+  description = "The number of web servers"
+  default     = "2"
+  type        = "string"
+}
+
+variable "digitalocean_datacenter" {
+  #default = "nyc1"
+  #default = "lon1"
+  #default = "ams3"
+  default = "ams2"
+
+  description = "Digital Ocean datacenter to use"
+  type        = "string"
+}
+
 # Create SSH key that can be used by droplets
 resource "digitalocean_ssh_key" "ssh" {
   name       = "NDC Oslo 2017"
@@ -10,9 +26,10 @@ resource "digitalocean_ssh_key" "ssh" {
 
 # Create droplet based on centos image
 resource "digitalocean_droplet" "web" {
+  count    = "${var.nb_web_servers}"
   image    = "centos-7-x64"
-  name     = "web"
-  region   = "ams2"
+  name     = "web${count.index}"
+  region   = "${var.digitalocean_datacenter}"
   size     = "512mb"
   ssh_keys = ["${digitalocean_ssh_key.ssh.id}"]
 }
