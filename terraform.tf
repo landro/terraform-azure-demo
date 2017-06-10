7feb92b3cdeb2f26fe955224bf4e5f5cbbb51d20
Stefan Magnus Landrø <stefan.landro@gmail.com>
2017-06-10 13:52:38 +0200

    Add ssh key

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,9 @@
 # Configure the DigitalOcean Provider
 # Will use DIGITALOCEAN_TOKEN environment variable
 provider "digitalocean" {}
+
+# Create SSH key that can be used by droplets
+resource "digitalocean_ssh_key" "ssh" {
+  name       = "NDC Oslo 2017"
+  public_key = "${file("ndc_id_rsa.pub")}"
+}
